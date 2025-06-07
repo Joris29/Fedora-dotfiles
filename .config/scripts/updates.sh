5eb80dd0ac465b47c447f339a5304ec8255474a7
Joris VAN ACOLEYEN <joris.vanacoleyen@sfpd.fgov.be>
2025-06-07 20:37:27 +0200

    Add common dotfiles

---
+++ .config/scripts/updates.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+action=$1
+
+case "$action" in
+    check)
+        updates=$(sudo dnf check-update --refresh -q | grep -c '^[a-z0-9]')
+
+        if [ "$updates" -gt 0 ]; then
+            printf '{"text": "%s", "alt": "%s", "tooltip": "Click to update your system"}' "$updates" "$updates"
+        else
+            printf '{"text": "0", "alt": "0", "tooltip": "No updates available"}'
+        fi
+        ;;
+    update)
+        kitty sh -c 'echo "Install updates" && sudo dnf update --refresh -q && pkill waybar && hyprctl dispatch exec waybar'
+        ;;
+    *)
+        echo "Invalid action specified. Valid options are: check and update"
+        exit 1
+        ;;
+esac
