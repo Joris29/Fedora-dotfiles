bd3674504cf39b21e20ad126322742e4365ac718
Joris VAN ACOLEYEN <joris.vanacoleyen@sfpd.fgov.be>
2025-06-12 14:44:59 +0200

    Add extra config

--- .config/scripts/keybinds.sh
+++ .config/scripts/keybinds.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-OUT="$HOME/.config/hypr/keybinds.conf"
-KB_DIR="$HOME/.config/hypr/kb"
+OUT="$HOME/.config/hypr/keybinds-extra.conf"
+DIR="$HOME/.config/hypr/kb"
 
-echo "# Auto-generated includes for keybinds" >> "$OUT"
-for file in "$KB_DIR"/*.conf; do
-    echo "source = $file" >> "$OUT"
-done
+FILES=("$DIR"/*.conf)
+
+if [[ -e "${FILES[0]}" ]]; then
+    echo "# Auto-generated includes for extra keybinds" > "$OUT"
+    for file in "${FILES[@]}"; do
+        echo "source = $file" >> "$OUT"
+    done
+fi
