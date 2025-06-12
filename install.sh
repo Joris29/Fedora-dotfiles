dec762ec9e36bce1756efb7372d6f18a5fc05d89
Joris VAN ACOLEYEN <joris.vanacoleyen@sfpd.fgov.be>
2025-06-12 17:02:21 +0200

    Update install script

--- install.sh
+++ install.sh
@@ -32,7 +32,18 @@ declare -a DNF_PKGS=(
     'wtype'
 )
 
-sudo dnf update -y
+sudo tee /etc/yum.repos.d/vscode.repo > /dev/null <<EOF
+[code]
+name=Visual Studio Code
+baseurl=https://packages.microsoft.com/yumrepos/vscode
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.microsoft.com/keys/microsoft.asc
+EOF
+
+sudo dnf config-manager addrepo -y --from-repofile="https://copr.fedorainfracloud.org/coprs/solopasha/hyprland/repo/fedora-$(rpm -E %fedora)/solopasha-hyprland-fedora-$(rpm -E %fedora).repo"
+
+sudo dnf check-update
 sudo dnf install -y "${DNF_PKGS[@]}"
 
 stow fedora -d "$HOME/dotfiles" -t "$HOME" --ignore=install.sh
