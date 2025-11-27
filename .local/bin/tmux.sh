b0e70c3de0647df60bf16a155f8c2acc7f655da6
Joris29 <joris.vanacoleyen@bryxx.eu>
2025-11-27 11:06:23 +0100

    Simplify floating tmux

--- .local/bin/tmux.sh
+++ .local/bin/tmux.sh
@@ -1,21 +1,12 @@
 #!/bin/bash
 
-TITLE="special_tmux"
-SWS="special:tmux"
-WIN_WS=$(hyprctl clients -j | jq -r '.[] | select(.title == "'"$TITLE"'") | .workspace.name')
-CURRENT_WS=$(hyprctl monitors -j | jq -r '.[] | select(.focused == true) | .activeWorkspace.id')
+TMUXSP=$(hyprctl clients -j | jq -r '.[] | select(.title=="tmuxsp")')
 
-if [ -z "$WIN_WS" ]; then
-  kitty --title "$TITLE" -e tmux new-session -ADs code
+if [ -z "$TMUXSP" ]; then
+  kitty --title tmuxsp -e tmux new-session -ADs code
   exit
 fi
 
-if [ "$WIN_WS" == "$SWS" ]; then
-  hyprctl dispatch movetoworkspace "$CURRENT_WS",title:"$TITLE"
-  hyprctl dispatch resizewindowpixel exact 100 100,title:"$TITLE"
-  hyprctl dispatch workspace "$CURRENT_WS"
-  hyprctl dispatch resizewindowpixel exact 80% 80%,title:"$TITLE"
-  hyprctl dispatch centerwindow
-else
-  hyprctl dispatch movetoworkspacesilent "$SWS",title:"$TITLE"
-fi
+hyprctl dispatch togglespecialworkspace tmuxsp
+hyprctl dispatch resizewindowpixel exact 80% 80%,title:tmuxsp
+hyprctl dispatch centerwindow
